1e0eade82bfd6a422d8233bac79b4c4addd01ef2
TheMickeyMike <macnow@st.amu.edu.pl>
2015-12-04 22:42:37 +0100

    Add concurrency in Sentiment analysis

--- src/utils/ReviewSentiment.java
+++ src/utils/ReviewSentiment.java
@@ -12,10 +12,15 @@ import model.Sentiment;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Maciej on 04.12.15.
  */
+
+//http://nlp.stanford.edu/software/corenlp-faq.shtml#memory
 public class ReviewSentiment {
 
     Sentiment sentimentCoef = null;
@@ -25,49 +30,63 @@ public class ReviewSentiment {
     int negative = 0;
     int veryNegative = 0;
 
-    Properties props;
-    StanfordCoreNLP pipeline;
-    Annotation annotation;
+    private Properties props;
+    private StanfordCoreNLP pipeline;
+    private Annotation annotation;
+    private ExecutorService pool;
 
     public ReviewSentiment() {
         this.props = new Properties();
         props.put("sentiment.model","/Users/Maciej/Desktop/IMDb-scraper/lib/stanford-corenlp-full-2015-04-20/models/model-0009-79,68.ser.gz");
         props.put("annotators", "tokenize, ssplit, pos, lemma, parse, sentiment");
         this.pipeline = new StanfordCoreNLP(props);
+        int availableProcessors = Runtime.getRuntime().availableProcessors();
+        this.pool = Executors.newFixedThreadPool(availableProcessors);
 
     }
 
     public void getReviewSentiment(ArrayList<Movie> movieArrayList) {
         for (Movie movie : movieArrayList) {
             for (Review review : movie.getReviews()) {
-                resetStats(); // Reset Stats
-                Annotation annotation = pipeline.process(review.getText());
-                List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
-                for (CoreMap sentence : sentences) {
-                    String sentiment = sentence.get(SentimentCoreAnnotations.SentimentClass.class);
-                    switch (sentiment) {
-                        case "Very positive":
-                            veryPositive++;
-                            break;
-                        case "Positive":
-                            positive++;
-                            break;
-                        case "Neutral":
-                            neutral++;
-                            break;
-                        case "Negative":
-                            negative++;
-                            break;
-                        case "Very negative":
-                            veryNegative++;
-                            break;
-                        default:
-                            break;
+                pool.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        resetStats(); // Reset Stats
+                        Annotation annotation = pipeline.process(review.getText());
+                        List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+                        for (CoreMap sentence : sentences) {
+                            String sentiment = sentence.get(SentimentCoreAnnotations.SentimentClass.class);
+                            switch (sentiment) {
+                                case "Very positive":
+                                    veryPositive++;
+                                    break;
+                                case "Positive":
+                                    positive++;
+                                    break;
+                                case "Neutral":
+                                    neutral++;
+                                    break;
+                                case "Negative":
+                                    negative++;
+                                    break;
+                                case "Very negative":
+                                    veryNegative++;
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        review.setSentiments(new Sentiment(veryPositive, positive, neutral, negative, veryNegative));
+                        System.out.println(veryPositive + " " + positive + " " + neutral + " " + negative + " " + veryNegative);
                     }
-                }
-                review.setSentiments(new Sentiment(veryPositive, positive, neutral, negative, veryNegative));
-                System.out.println(veryPositive + " " + positive + " " + neutral + " " + negative + " " + veryNegative);
+                });
             }
+        }
+        pool.shutdown();
+        try {
+            pool.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
